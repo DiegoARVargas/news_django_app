b25be576686b6b97efeac9e83629115ebeeb1119
diego <rodriguezvargasd214@gmail.com>
2023-12-20 22:41:48 -0800

    comments app

--- articles/admin.py
+++ articles/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Comment
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0   # Esta variable la utilizacion para indicar el # de filas extra que deseamos que aparezca en el adminsite por default.
 
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
     list_display = [
         "title",
         "body",
@@ -9,3 +16,4 @@ class ArticleAdmin(admin.ModelAdmin):
     ]
 
 admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
--- articles/views.py
+++ articles/views.py
@@ -1,16 +1,67 @@
+from typing import Any
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import ListView, DetailView
+from django.views import View
+from django.views.generic import ListView, DetailView, FormView
+from django.views.generic.detail import SingleObjectMixin
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from .models import Article
+from .forms import CommentForm
 
 class ArticleListView(LoginRequiredMixin, ListView):
     model = Article
     template_name = "article_list.html"
 
-class ArticleDetailView(LoginRequiredMixin, DetailView):
+class CommentGet(DetailView):
     model = Article
     template_name = "article_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = CommentForm()
+        return context
+
+'''
+SingleObjectMixin: Proporciona métodos para obtener el objeto sobre el cual operará la vista. 
+                   Por ejemplo, get_object() se utiliza para recuperar el objeto basado en la 
+                   URL o en otros parámetros.
+'''
+class CommentPost(SingleObjectMixin, FormView):
+    model = Article
+    form_class = CommentForm
+    template_name = "article_detail.html"
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()     #get_object() para obtener un objeto específico con pk según los parámetros de la URL
+        return super().post(request, *args, **kwargs)
+    
+    def form_valid(self, form):
+        comment = form.save(commit=False)   #Se crea una instancia del modelo Comment a partir de los datos del formulario pero no se guarda en la BD aun (commit=False)
+        comment.article = self.object   #Se asigna el artículo asociado al comentario
+        comment.author = self.request.user  #Se asigna el autor del comentario, que es el usuario actual que realizó la solicitud
+        comment.save()
+        return super().form_valid(form)
+    
+    '''
+    cuando llamamos super().form_valid(form) dentro del método form_valid, estamos invocando al método form_valid de la clase base (FormView). Esto es parte de la herencia de clases en Python.
+    El método form_valid() de la clase FormView es responsable de manejar la lógica cuando un formulario es válido. Al llamar super().form_valid(form), estamos delegando esa lógica a la implementación proporcionada por la clase base (FormView).
+    Esta llamada tiene un propósito importante: permite que la lógica adicional de FormView se ejecute después de nuestra propia lógica personalizada en form_valid. La clase base FormView se encarga de tareas como redireccionamiento, mensajes de éxito o cualquier otra lógica definida en ella.
+    En resumen, al llamar a super().form_valid(form), se ejecuta el código en la clase FormView asociado al manejo de formularios válidos. Esto asegura que cualquier funcionalidad adicional proporcionada por FormView o sus clases base se ejecute después de la lógica personalizada definida en form_valid dentro de la vista CommentPost.
+    '''
+    
+    def get_success_url(self):
+        article = self.object
+        return reverse("article_detail", kwargs={"pk": article.pk})
+
+class ArticleDetailView(LoginRequiredMixin, View):
+    
+    def get(self, request, *args, **kwargs):
+        view = CommentGet.as_view()
+        return view(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        view = CommentPost.as_view()
+        return view(request, *args, **kwargs)
 
 class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Article
